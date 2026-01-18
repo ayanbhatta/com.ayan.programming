8af02ad8dba61dc8d820452ce5612cf4c643d7cf
ayanbhatta <ayanb8127@gmail.com>
2026-01-18 23:19:38 +0530

    Hard level pattern

---
+++ src/com/ayan/programming/pattern/AdvancePattern.java
@@ -0,0 +1,16 @@
+package com.ayan.programming.pattern;
+
+public class AdvancePattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(i==n/2+1 || j==n/2+1 || j==1&&i<=n/2+1 || i==1 && j>=n/2+1 || i==n &&j<=n/2+1 || j==n&&i>=n/2+1)    System.out.print("* ");
+                else    System.out.print("  ");
+            }System.out.println();
+        }
+	}
+
+}
