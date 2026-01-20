b4bbe297c695f1800289db2f8ea593e373e2c66e
ayanbhatta <ayanb8127@gmail.com>
2026-01-20 23:27:46 +0530

    occurance in array

---
+++ src/com/ayan/programming/array/Occurance.java
@@ -0,0 +1,18 @@
+package com.ayan.programming.array;
+
+public class Occurance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr= {10,20,10,30,20};
+		for(int i=0;i<arr.length;i++) {
+			int count = 1;
+			for(int j=i+1;j< arr.length;j++) {
+				if(arr[i]==arr[j]) {
+					count++;
+				}
+			}System.out.println(arr[i]+"->"+count);
+		}
+	}
+
+}
